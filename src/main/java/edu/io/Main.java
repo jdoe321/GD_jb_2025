1cefc8a3d9412f8b9ab473e0340ed9d53a20b2bf
Joanna Bendkowska <joanna.bendkowska@o365.us.edu.pl>
2025-11-14 21:19:51 +0100

    Modyfikacja Main do testow

--- src/main/java/edu/io/Main.java
+++ src/main/java/edu/io/Main.java
@@ -4,6 +4,13 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        System.out.println("Gold Rush");
+        Board board = new Board();
+        Token playerToken = new Token(board.PLAYER_TOKEN_LABEL);
+        Token goldToken = new Token(board.GOLD_TOKEN_LABEL);
+
+        board.grid[2][3] = playerToken;
+        board.grid[5][6] = goldToken;
+
+        board.display();
     }
 }
