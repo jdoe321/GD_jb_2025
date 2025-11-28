1103ca64991c3f96ba28712c73c9f39651905822
Joanna Bendkowska <joanna.bendkowska@o365.us.edu.pl>
2025-11-28 23:54:55 +0100

    Implementacja zbierania zlota

--- src/main/java/edu/io/Player.java
+++ src/main/java/edu/io/Player.java
@@ -5,15 +5,18 @@ import edu.io.token.PlayerToken;
 public class Player 
 {
     public PlayerToken token;
+    private double gold;
 
     public Player()
     {
         this.token = null;
+        gold = 0.0;
     }
 
     public Player(PlayerToken token) 
     {
         this.assignToken(token);
+        gold = 0.0;
     }
 
     public void assignToken(PlayerToken token) 
@@ -24,6 +27,26 @@ public class Player
     public PlayerToken token() 
     {
         return token;
+    }
+
+    public double gold() 
+    {
+        return gold;
+    }
+
+    public void gainGold(double amount) 
+    {
+        gold += amount;
+    }
+
+    public void loseGold(double amount) 
+    {
+        gold -= amount;
+        if (gold < 0) 
+        {
+            gold = 0;
+            throw new IllegalArgumentException("zabraklo zlota");
+        }
     }
     
 }
--- src/main/java/edu/io/token/GoldToken.java
+++ src/main/java/edu/io/token/GoldToken.java
@@ -2,9 +2,23 @@ package edu.io.token;
 
 public class GoldToken extends Token 
 {
+    private double amount;
+
     public GoldToken() 
     {
         super(Label.GOLD_TOKEN_LABEL);
+        this.amount = 100.0;
+    }
+
+    public GoldToken(double amount) 
+    {
+        super(Label.GOLD_TOKEN_LABEL);
+        this.amount = amount;
+    }
+
+    public double amount() 
+    {
+        return amount;
     }
     
 }
